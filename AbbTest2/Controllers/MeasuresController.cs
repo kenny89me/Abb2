6178fd7ef977a5aec02490455cd5e545b7f5642b
yar.gord8ASdYa9KEnny <yar.gorda asczcvxasd89@gmail.com>
2019-02-09 13:56:30 +0200

    Added WCF

---
+++ AbbTest2/Controllers/MeasuresController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AbbTest2.DAL;
+using AbbTest2.Models;
+
+namespace AbbTest2.Controllers
+{
+    public class MeasuresController : Controller
+    {
+        private AbbTest2Context db = new AbbTest2Context();
+
+        // GET: Measures
+        public ActionResult Index()
+        {
+            var measures = db.Measures.Include(m => m.Motor); 
+            foreach (var mes in measures)
+            {
+                mes.CurrentA = mes.Motor.CurrentA;
+                mes.MaxTorque = mes.Motor.MaxTorque;
+                mes.MaxPressure = mes.Motor.MaxPressure;
+
+                switch (mes.MotorId)
+                {
+                    case 1:
+                        mes.Difference = mes.CurrentA - mes.ActualCurrentA;
+                        break;
+
+                    case 2:
+                        mes.Difference = mes.MaxTorque - mes.ActualRevsRpm;
+                        break;
+
+                    case 3:
+                        mes.Difference = mes.MaxPressure - mes.ActualPressureBar;
+                        break;
+                }
+   
+            }
+            db.SaveChanges();
+
+            return View(measures.ToList());
+        }
+
+        // GET: Measures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Measure measure = db.Measures.Find(id);
+            if (measure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measure);
+        }
+
+        // GET: Measures/Create
+        public ActionResult Create()
+        {
+            ViewBag.MotorId = new SelectList(db.Motors, "MotorId", "MotorName");
+            return View();
+        }
+
+        // POST: Measures/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MeasureId,Timestamp,MotorId,MotorName,CurrentA,MaxTorque,MaxPressure,ActualCurrentA,ActualRevsRpm,ActualPressureBar,Difference")] Measure measure)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Measures.Add(measure);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MotorId = new SelectList(db.Motors, "MotorId", "MotorName", measure.MotorId);
+            return View(measure);
+        }
+
+        // GET: Measures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Measure measure = db.Measures.Find(id);
+            if (measure == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MotorId = new SelectList(db.Motors, "MotorId", "MotorName", measure.MotorId);
+            return View(measure);
+        }
+
+        // POST: Measures/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MeasureId,Timestamp,MotorId,MotorName,CurrentA,MaxTorque,MaxPressure,ActualCurrentA,ActualRevsRpm,ActualPressureBar,Difference")] Measure measure)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(measure).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MotorId = new SelectList(db.Motors, "MotorId", "MotorName", measure.MotorId);
+            return View(measure);
+        }
+
+        // GET: Measures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Measure measure = db.Measures.Find(id);
+            if (measure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measure);
+        }
+
+        // POST: Measures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Measure measure = db.Measures.Find(id);
+            db.Measures.Remove(measure);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- AbbTest2/DAL/AbbTest2Context.cs
+++ AbbTest2/DAL/AbbTest2Context.cs
@@ -4,7 +4,9 @@ using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using AbbTest2.Models;
+using System.Data.Entity.Model;
 
 namespace AbbTest2.DAL
 {
